fe0effb5b2acfa941c9fb42a6f2f135f5d0b680c
doquochuy <doquochuyy66@gmail.com>
2024-12-17 20:26:06 +0700

    interface list product ADmin

--- src/main/webapp/js/LoadDataProduct.js
+++ src/main/webapp/js/LoadDataProduct.js
@@ -1,13 +1,13 @@
-function loadProductPage() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/view/ListProduct.jsp', true);
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            document.getElementById('content').innerHTML = xhr.responseText;
-        }
-    };
-    xhr.send();
-}
+// function loadProductPage() {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('GET', '/view/ListProduct.jsp', true);
+//     xhr.onload = function() {
+//         if (xhr.status === 200) {
+//             document.getElementById('content').innerHTML = xhr.responseText;
+//         }
+//     };
+//     xhr.send();
+// }
 function loadHome(){
     document.getElementById('content').innerHTML = `
         <img src="https://gotrangtri.vn/wp-content/uploads/2020/06/b%C3%ACa3.jpg" 
